73026cba0b6517f6eee5f0f405a8ba4ee108407f
jonasnascimento <jonas.soares.nascimento@gmail.com>
2023-12-04 12:56:49 -0300

    adding-comparison-operator

--- scripts.js
+++ scripts.js
@@ -2,12 +2,15 @@ const generateNumber = document.querySelector("#button-generate-number")
 
 
 generateNumber.addEventListener("click", buttongeneratenumber)
-function buttongeneratenumber (){
-    const min = Math.ceil( document.querySelector (".input-entre").value)
-    const max = Math.floor( document.querySelector (".input-e").value)
+function buttongeneratenumber() {
+    const min = Math.ceil(document.querySelector(".input-entre").value)
+    const max = Math.floor(document.querySelector(".input-e").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1) + min)
-alert (result)
-    
+    if (min >= max) {
+        alert("O valor de (entre) tem que ser MENOR que (e)")
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1) + min)
+        alert(result)
+    }
 }
 
